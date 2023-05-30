95101db1750d699db7abf3396a7e96c0a9aa8f24
hoanghao94 <hoanghao160894@gmail.com>
2023-05-30 23:49:23 +0700

    Updates

--- server.js
+++
@@ -1,8 +0,0 @@
-const cors_proxy = require('cors-anywhere');
-
-// Cấu hình và khởi động proxy trên cổng 8081
-cors_proxy.createServer({
-  originWhitelist: [], // Cho phép tất cả các nguồn
-}).listen(8081, 'localhost', function() {
-  console.log('CORS Anywhere proxy đã được khởi động trên cổng 8081...');
-});
--- src/actions/loginAction.js
+++
@@ -1,8 +0,0 @@
-
-export const setLoginData = (username, password) => {
-    return {
-      type: 'SET_LOGIN_DATA',
-      payload: { username, password },
-    };
-  };
-  
